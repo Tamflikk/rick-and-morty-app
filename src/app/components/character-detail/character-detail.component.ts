42e23d2907efdea3ad291051e9f2f1394525990c
Tamflikk <alejandrotamayo32200@gmail.com>
2025-04-25 20:33:41 -0400

    update character-detail.component.ts

--- src/app/components/character-detail/character-detail.component.ts
+++ src/app/components/character-detail/character-detail.component.ts
@@ -7,7 +7,6 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-character-detail',
   templateUrl: './character-detail.component.html',
-  styleUrls: ['./character-detail.component.scss']
 })
 export class CharacterDetailComponent implements OnInit {
   character!: Character;
